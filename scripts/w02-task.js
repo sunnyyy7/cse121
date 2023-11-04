3004fa383d85a6763898cc201cf2551ef2a05a9f
yourusername <youremail@>
2023-11-03 20:27:35 -0600

    Updated Week2-js Task & old index gtml set up

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,23 +4,38 @@
 
 /* Step 2 - Variables */
 
-
+let fullName = 'Sunny Yeung';
+let currentyear = 2023;
+let profilePicture = 'images/IMG_1204.jpg';
 
 
 /* Step 3 - Element Variables */
 
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
 
-
-
+const imageElement = document.querySelector('img')
 
 /* Step 4 - Adding Content */
 
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
+yearElement.textContent = currentyear;
 
-
-
-
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `profile image of ${fullName}`);
 /* Step 5 - Array */
+
+let favFoods = ['DimSum', 'Fried Rice', 'Noddles', 'Pizza', 'Salad']
+foodElement.innerHTML = favFoods.join();
+Food1 = 'Burrito';
+favFoods.push(Food1);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
 
 
 
